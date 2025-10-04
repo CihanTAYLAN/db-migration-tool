0fcf9f24c7cfac113ef2e611e4ac19c45ee035d2
Cihan TAYLAN <cihantaylan@cihantaylan.com>
2025-10-04 12:42:29 +0300

    description fixed

--- src/migrationsv3/steps/products.js
+++ src/migrationsv3/steps/products.js
@@ -233,7 +233,7 @@ class ProductsStep {
                 cpe.sku as product_sku,
                 COALESCE(cpevs_name.value, cpf.name, cpe.sku) as name,
                 COALESCE(cped.value, cpf.price) as price,
-                cpf.description as description,
+                COALESCE(cpet_desc.value, cpf.description) as description,
                 cpf.short_description as short_description,
                 cpf.image as image,
                 cpf.url_key as url_key,
@@ -264,6 +264,7 @@ class ProductsStep {
             LEFT JOIN catalog_product_entity_varchar cpevs_meta_desc ON cpe.entity_id = cpevs_meta_desc.entity_id AND cpevs_meta_desc.attribute_id = 86 AND cpevs_meta_desc.store_id = 0
             LEFT JOIN catalog_product_entity_text cpet_cert ON cpe.entity_id = cpet_cert.entity_id AND cpet_cert.attribute_id = 138 AND cpet_cert.store_id = 0
             LEFT JOIN catalog_product_entity_text cpet_coin ON cpe.entity_id = cpet_coin.entity_id AND cpet_coin.attribute_id = 142 AND cpet_coin.store_id = 0
+            LEFT JOIN catalog_product_entity_text cpet_desc ON cpe.entity_id = cpet_desc.entity_id AND cpet_desc.attribute_id = 75 AND cpet_desc.store_id = 0
             LEFT JOIN catalog_product_entity_varchar cpevs_country_manuf ON cpe.entity_id = cpevs_country_manuf.entity_id AND cpevs_country_manuf.attribute_id = 114 AND cpevs_country_manuf.store_id = 0
             LEFT JOIN catalog_product_entity_varchar cpevs_grade_prefix ON cpe.entity_id = cpevs_grade_prefix.entity_id AND cpevs_grade_prefix.attribute_id = 135 AND cpevs_grade_prefix.store_id = 0
             LEFT JOIN catalog_product_entity_decimal cped_grade_value ON cpe.entity_id = cped_grade_value.entity_id AND cped_grade_value.attribute_id = 148 AND cped_grade_value.store_id = 0
@@ -293,7 +294,7 @@ class ProductsStep {
             WHERE ${whereClause}
             GROUP BY cpe.entity_id, cpe.sku, cpevs_name.value, cped.value, cpf.name, cpf.price, cpf.description, cpf.short_description,
                      cpf.image, cpf.url_key, cpe.created_at, cpe.updated_at, cpevs_meta_title.value,
-                     cpevs_meta_desc.value, cpet_cert.value, cpet_coin.value, cpevs_country_manuf.value, cpevs_grade_prefix.value,
+                     cpevs_meta_desc.value, cpet_cert.value, cpet_coin.value, cpet_desc.value, cpevs_country_manuf.value, cpevs_grade_prefix.value,
                      cped_grade_value.value, cpevs_grade_suffix.value, cpf.year, cpf.country, cpf.country_value, cpei_cert_type.value, sold_dates.first_sale_date, sold_prices.last_sold_price, cpev_sort.value
             ORDER BY cpe.entity_id
         `;
